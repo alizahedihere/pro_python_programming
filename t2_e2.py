ba1dabeb539187e2897a4b71f2d01d73a714837b
Ali Zahedi Sohrofrouzani <75866665+alizahedihere@users.noreply.github.com>
2024-02-08 14:00:00 +0330

    Create t2_e2.py

---
+++ t2_e2.py
@@ -0,0 +1,6 @@
+def multi(): 
+ multiply = first_num*second_num*third_num  return multiply 
+def div(): 
+ division = (first_num // second_num)  return division 
+first_num = int(input("Enter First number :")) second_num = int(input("Enter Second number :")) third_num = int(input("Enter third number :")) 
+print("Multiply Of Numbers is : ", multi()) print("Floor Division is : ", div()) 
