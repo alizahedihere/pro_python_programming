eab7e2eba0e1df370559265951e8a0d67451e6fa
Ali Zahedi Sohrofrouzani <75866665+alizahedihere@users.noreply.github.com>
2024-02-08 13:53:58 +0330

    Create t1_e1.py

---
+++ t1_e1.py
@@ -0,0 +1,11 @@
+def s(x): 
+ s_n = P*x*x 
+ return s_n 
+def p(y): 
+ p_n = 2*P*y 
+ return p_n 
+print("Hello") 
+P = 3 
+shoa = int(input("Enter Shoa of the Circle :")) 
+print("Masahat is :", s(shoa)) 
+print("Mohit of the Circle :", p(shoa)) 
