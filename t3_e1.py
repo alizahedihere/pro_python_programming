4740ea397ca1e83240848809a53daf8dd1702506
Ali Zahedi Sohrofrouzani <75866665+alizahedihere@users.noreply.github.com>
2024-02-08 13:58:33 +0330

    Create t3_e1.py

---
+++ t3_e1.py
@@ -0,0 +1,7 @@
+multiply = 1 
+print("Hello") 
+while 1: 
+ num = input("Enter Number (for Stop type end): ")  if num == "end": 
+ break 
+ multiply *= int(num) 
+print("Multiply of Numbers is :", multiply) 1
