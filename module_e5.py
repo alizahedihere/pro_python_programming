eaeabe6b1d6e0dd9bb46337497292b2964092bc6
Ali Zahedi Sohrofrouzani <75866665+alizahedihere@users.noreply.github.com>
2024-02-11 17:46:12 +0330

    Create module_e5.py

---
+++ module_e5.py
@@ -0,0 +1,6 @@
+def mohit_core(r): 
+ return 4*3.14*r*r 
+def hajm_core(h): 
+ return (4/3)*3.14*(h*h*h) def mohit_zozanagheh(a,b,c,d):  return a+b+c+d 
+def masahat_mosalas_ghaem(o,p):  return (o*p)/2 
+1
